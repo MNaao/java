1ea9613060c46b86e10b577930240b2d3504da18
김성현 <121427277+MNaao@users.noreply.github.com>
2023-02-28 09:41:38 +0900

    Add files via upload
    2023/02/28 김성현 JAVA

---
+++ ex01_출력.java
@@ -0,0 +1,17 @@
+package ex1028;
+
+public class ex01_출력 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!"); // 실행 -> F11
+		
+		System.out.print("이름 : ");
+		
+		System.out.println("김성현");
+		
+		
+		
+	}
+
+}
